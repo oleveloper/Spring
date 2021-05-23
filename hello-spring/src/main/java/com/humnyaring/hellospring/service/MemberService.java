a5310b2687102a4b104546f073de4e07aad3b5bd
oleveloper <oleveloper@gmail.com>
2021-05-23 23:21:52 +0900

    Member service test

--- hello-spring/src/main/java/com/humnyaring/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/humnyaring/hellospring/service/MemberService.java
@@ -2,14 +2,19 @@ package com.humnyaring.hellospring.service;
 
 import com.humnyaring.hellospring.domain.Member;
 import com.humnyaring.hellospring.repository.MemberRepository;
-import com.humnyaring.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     /* 회원 가입 */
     public Long join(Member member) {
