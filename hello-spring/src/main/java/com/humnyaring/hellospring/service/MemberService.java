0e58a2c4bcbdb14d2e5deda1cdfb828f7cb0d0b4
oleveloper <oleveloper@gmail.com>
2021-05-24 23:13:04 +0900

    Component scan and Autowired

--- hello-spring/src/main/java/com/humnyaring/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/humnyaring/hellospring/service/MemberService.java
@@ -2,15 +2,19 @@ package com.humnyaring.hellospring.service;
 
 import com.humnyaring.hellospring.domain.Member;
 import com.humnyaring.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
