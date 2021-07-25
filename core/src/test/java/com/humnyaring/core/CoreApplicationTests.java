1a26290236c0553c21656b9bb274523e7e28e5e1
oleveloper <oleveloper@gmail.com>
2021-07-25 17:07:49 +0900

    Initial commit

---
+++ core/src/test/java/com/humnyaring/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humnyaring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
