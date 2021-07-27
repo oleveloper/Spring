ee921abf0d4216d7f06d11451c1d78d15a5da25e
oleveloper <oleveloper@gmail.com>
2021-07-27 12:01:12 +0900

    Implement Order and Discount domain test code

---
+++ core/src/main/java/com/humnyaring/core/order/OrderApp.java
@@ -0,0 +1,21 @@
+package com.humnyaring.core.order;
+
+import com.humnyaring.core.member.Grade;
+import com.humnyaring.core.member.Member;
+import com.humnyaring.core.member.MemberService;
+import com.humnyaring.core.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
---
+++ core/src/test/java/com/humnyaring/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package com.humnyaring.core.order;
+
+import com.humnyaring.core.member.Grade;
+import com.humnyaring.core.member.Member;
+import com.humnyaring.core.member.MemberService;
+import com.humnyaring.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
